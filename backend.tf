2c0fb92656f28840025a4180be494ce7c72d7022
Joel Gray <32583151+pyjoepy06@users.noreply.github.com>
2025-02-11 12:46:26 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,17 @@
 terraform {
-  backend "remote" {
-    organization = "ConvergeOne"
-    hostname     = "app.terraform.io"
-    workspaces {
-      name = "centrailized-vpc-tgw-demo"
-    }
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
   }
 }
+
+# terraform {
+#   backend "remote" {
+#     organization = "<MyOrganization-Name>"
+#     hostname     = "app.terraform.io"
+#     workspaces {
+#       name = "centrailized-vpc-tgw-demo"
+#     }
+#   }
+# }
